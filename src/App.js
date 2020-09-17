8b851e982c2c8395cb5e8281b4a35a8b02627c8e
mdNaimulHoque <naimulhoq89@gmail.com>
2020-09-17 21:04:34 +0600

    country.js file is added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Country from './components/Country/Country';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+    .catch(error => console.error(error))
+  }, [])
   return (
     <div className="App">
+      <h1>country loaded: {countries.length}</h1>
+      <ul>
+          {
+            countries.map(country => <Country name={country.name}></Country>)
+          }
+      </ul>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
